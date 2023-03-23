123e6afe41caeddb07688215c0380cc29c52fec3
WilgnerJR <wilgnerbento@gmail.com>
2023-03-22 23:01:32 -0400

    update

--- js/slide.js
+++ js/slide.js
@@ -1,15 +1,9 @@
 let count = 1;
-document.getElementById('radio1').checked = true;
 
-setInterval (function(){
-    nextImg();
-},2000)
-
-
-function nextImg(){
-    count++;
-    if(count>4){
+setInterval( function(){
+    document.getElementById('slide' + count).checked = true;
+    count ++;
+    if (count > 4){
         count = 1;
     }
-    document.getElementById('radio'+count).checked = true;
-}
+},1700 );
